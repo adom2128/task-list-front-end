de484e435ab39a10d95a0181d4654f1c41f184f4
Alejandra <adom2128@gmail.com>
2023-06-22 08:42:41 -0600

    wave 3 passing with correct complete/incomplete endpoint and starts wave 4

--- src/App.js
+++ src/App.js
@@ -1,31 +1,63 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import TaskList from './components/TaskList.js';
 import './App.css';
+import NewTaskForm from './components/NewTaskForm.js';
 
-const TASKS = [
-  {
-    id: 1,
-    title: 'Mow the lawn',
-    isComplete: false,
-  },
-  {
-    id: 2,
-    title: 'Cook Pasta',
-    isComplete: true,
-  },
-];
+const baseUrl = 'http://127.0.0.1:5000';
+
+const getAllTasks = () => {
+  return axios
+    .get(`${baseUrl}/tasks`)
+    .then((response) => response.data.map(convertFromApi))
+    .catch((error) => console.log(error));
+};
+
+const convertFromApi = (apiCat) => {
+  const { description, id, is_complete: isComplete, title } = apiCat;
+  return {description, id, isComplete, title};
+};
 
 const App = () => {
-  const [taskData, setTaskData] = useState(TASKS);
+
+  const [taskData, setTaskData] = useState([]);
+
+  const fetchTasks = () => {
+    getAllTasks().then((tasks) => {
+      setTaskData(tasks);
+    });
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const markCompleteApi = (id, isCurrentlyComplete) => {
+    const endpoint = isCurrentlyComplete ? 'mark_incomplete' : 'mark_complete';
+
+    return axios
+      .patch(`${baseUrl}/tasks/${id}/${endpoint}`)
+      .then((response) => response.data.map(convertFromApi))
+      .catch((error) => console.log(error));
+  };
 
   const markComplete = (id) => {
     const tasks = taskData.map(task => {
       if (task.id === id) {
+        const completionState = task.isComplete;
         task.isComplete = !task.isComplete;
+        markCompleteApi(id, completionState);
       }
       return task;
     });
     setTaskData(tasks);
+  };
+
+  const deleteTaskApi = (id) => {
+    return axios
+      .delete(`${baseUrl}/tasks/${id}`)
+      .then((response) => response.data.map(convertFromApi))
+      .catch((error) => console.log(error));
   };
 
   const deleteTask = (id) => {
@@ -34,9 +66,21 @@ const App = () => {
         return task;
       }
     });
-    // setTaskData(taskData.filter((task) => task.id !== id));
+
     setTaskData(tasks);
+    deleteTaskApi(id);
   };
+
+
+  const handleSubmit = (newTask) => {
+    axios
+      .post(`${baseUrl}/tasks`, newTask)
+      .then((response) => {
+        setTaskData((prev) => [convertFromApi(response.data), ...prev]);
+      })
+      .catch((error) => console.log(error));
+  };
+
 
   return (
     <div className="App">
@@ -44,11 +88,16 @@ const App = () => {
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div><TaskList 
-          tasks={taskData} 
-          onSetComplete={markComplete} 
-          onDeleteTask={deleteTask}
-        /></div>
+        <div>
+          <TaskList 
+            tasks={taskData} 
+            onSetComplete={markComplete} 
+            onDeleteTask={deleteTask}
+          />
+          <NewTaskForm 
+            onHandleSubmit={handleSubmit}
+          />
+        </div>
       </main>
     </div>
   );
---
+++ src/components/NewTaskForm.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './NewTaskForm.css';
+
+const NewTaskForm = ({ onHandleSubmit }) => {
+
+    const [newTaskName, setNewTaskName] = useState('');
+
+    const handleTaskNameChange = (event) => {
+        setNewTaskName(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const newTask = {
+            title: '',
+            description: newTaskName,
+            isComplete: false,
+        };
+
+        onHandleSubmit(newTask);
+        setNewTaskName('');
+    };
+
+    return (
+        <form className="new-task-form" onSubmit={handleSubmit}>
+            <label htmlFor="newTaskName">New Task:</label>
+            <input type="text" id="newTaskName" name="newTaskName" onChange={handleTaskNameChange} value={newTaskName}/>
+            <input type="submit" value="add task" />
+        </form>
+    );
+};
+
+NewTaskForm.propTypes = {
+    onHandleSubmit: PropTypes.func.isRequired,
+};
+
+export default NewTaskForm;
